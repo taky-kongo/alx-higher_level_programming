4130ed8f561eff6fab926c7c4ecfeee418b27e29
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2023-12-11 11:42:50 +0000

    Update the file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None or matrix == [[]]:
-        return None
+        print()
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if len(matrix[i]) - 1 != j:
