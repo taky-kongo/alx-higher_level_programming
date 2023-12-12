96f7a041f8e2d500f912f43f2234928ed4812de5
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2023-12-12 19:21:10 +0000

    Add the file

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if my_list == []:
+        return None
     from functools import reduce
     result = (reduce(lambda x, y: x + y, list(set(my_list))))
     return result
