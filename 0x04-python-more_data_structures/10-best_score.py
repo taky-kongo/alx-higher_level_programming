7270ec01d3b4b459094512081abc91ff8ef924ca
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2023-12-13 18:02:52 +0000

    Update the file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,8 +4,8 @@ def best_score(a_dictionary):
         return None
     score = 0
     for k, v in a_dictionary.items():
-        best_key = ""
         if v > score:
+            best_key = ""
             score = v
             best_key = best_key + k
     return best_key
