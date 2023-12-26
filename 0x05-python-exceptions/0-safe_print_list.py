608536a2831b8789f98ff389f28c4e148095f44a
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2023-12-26 15:09:23 +0000

    Add the file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     y = 0
-    for i in my_list:
-        if y == x:
-            break
-        else:
-            print(i, end="")
-        y += 1
-    print()
+    try:
+        for i in my_list:
+            if y == x:
+                break
+            else:
+                print(i, end="")
+            y += 1
+        print()
+    except NameError as nameErr:
+        print(nameErr)
     return y
