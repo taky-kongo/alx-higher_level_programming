e648d25acbe239508a2796c1557584eeef7160de
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2023-12-27 17:47:03 +0000

    Add the file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        print("{}".format("Exception:"), err)
+        sys.stderr.write("{}".format("Exception:"), err)
         return False
     else:
         return True
