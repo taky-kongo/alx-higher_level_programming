43210a4779045680af5fcbd91de17dbfbdb6cff2
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2024-01-01 17:02:32 +0000

    Add the file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a class that defines a square."""
+
+
+class Square:
+    """A class Square
+       Private instance attribute: size
+    """
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
