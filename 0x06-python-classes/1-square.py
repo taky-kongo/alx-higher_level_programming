f4011667643aa1bccb2d50ede9f98f54897ee313
Abdoul Kongo <abdoulmalickkongo@gmail.com>
2024-01-01 16:43:52 +0000

    Add te file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create a class that defines a square."""
+
+
+class Square:
+    """A class Square
+       Private instance attribute: size
+    """
+    def __init__(self, size):
+        self.__size = size
